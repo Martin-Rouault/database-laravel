489f127c8250c36178ffffd7c91dbddb76e9d557
Martin-Rouault <martin.rlt24@gmail.com>
2026-01-12 13:57:50 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Settings;
+
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            SettingsSeeder::class,
+        ]);
+    }
+}
+
+class SettingsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $user = User::where('email', 'test@example.com')->first();
+
+        $user->setting()->create([
+            'theme' => 'dark',
+            'lang'  => 'fr'
+        ]);
+    }
+}
