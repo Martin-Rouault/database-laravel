444f1b2c413e3a96e39b459763c877a35d1f11db
Martin-Rouault <martin.rlt24@gmail.com>
2026-01-13 10:25:02 +0100

    unique foreign key in migration

--- database/migrations/2026_01_12_203019_create_group_user_table.php
+++ database/migrations/2026_01_12_203019_create_group_user_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('group_id')->constrained();
+            $table->unique(['user_id', 'group_id']);
             $table->timestamps();
         });
     }
